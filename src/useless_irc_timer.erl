a5ad95a798f13159d829abea26175e0d73751ab3
Bassem Youssef <bassem@outlook.com>
2014-05-15 21:28:39 -0700

    Cleanup of timer plugin

--- src/useless_irc_timer.erl
+++ src/useless_irc_timer.erl
@@ -42,29 +42,58 @@ run_timer(_State = #worker_state{user=User,parent=ParentPid,
         %% have to delete timer from pending after it expires
     end.
 
+validate_timer_requested([H,M,S] = Timer) when 0 =< H andalso H =< 23 andalso
+                                               0 =< M andalso M =< 59 andalso
+                                               0 =< S andalso S =< 59 ->
+    {ok, Timer};
+
+validate_timer_requested([_H,_M,_S]) ->
+    {error, "Time specified is not within range"}.
+
+%% have to make sure m 0,59 and secs the same
+parse_timer_request(Request) ->
+    [RequestedTime | Msg] = string:tokens(Request," "),
+    try
+        {ok,
+         % make sure we're passed numbers
+         [erlang:list_to_integer(S) || S <- string:tokens(RequestedTime,":")],
+         string:strip(string:join(Msg," "))}
+    catch
+        _:_ -> {error, "Timer should be specified as numbers hh:mm:ss"}
+    end.
+
+handle_new_timer(Request) ->
+    case parse_timer_request(Request) of
+        {error, Msg} ->
+            {error, Msg};
+        {ok, HMS, Msg} ->
+            {ok, validate_timer_requested(HMS), Msg}
+    end.
+
 handle_info({run, Request, Chan, User, From}, State)
   when Request =:= "" orelse Request =:= "help" ->
     From ! {cmd_resp, User, Chan, "Set a timer in h:m:s format."},
     {noreply, State};
 
-handle_info({run, _Request, Chan, User, FromPid},
+%% Request should look like h:m:s some message
+handle_info({run, Request, Chan, User, FromPid},
             #state{pending=Pending} = State) when Chan =:= ""->
     case lists:keyfind(User,1,Pending) of
         %% check that user could be one of the active sessions
-        {_, Chan, _, _Worker} ->
+        {User, Chan, _, _Worker} ->
             %% try not accept another command if have one outstanding for that
             %% same user
             FromPid ! {cmd_resp, User, Chan, "You already have a timer running"},
             {noreply, State};
         _ ->
-            case length(Pending) >= ?MAX_PENDING_TIMERS of
-                true ->
-                    FromPid ! {cmd_resp, User, Chan, "Too many timers running"},
+            case handle_new_timer(Request) of
+                {error, Msg} ->
+                    FromPid ! {cmd_resp, User, Chan, Msg},
                     {noreply, State};
-                false ->
+                {ok, _HMS, Msg} ->
                     Worker = spawn_link(?MODULE, run_timer,
                                         [#worker_state{user = User,
-                                                       timer = 20, message = "Tea time",
+                                                       timer = 10, message = Msg,
                                                        parent = self()}]),
                     NewState = #state{pending = [{User, FromPid, Worker} | Pending]},
                     {noreply, NewState}
